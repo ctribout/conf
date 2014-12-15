b788b60970b42c33466e1cc15e23ade53d3d2b69
Soqaris <soqaris@gmail.com>
2014-12-15 15:58:50 +0100

    VIM initial configuration + script for symlinks

---
+++ install.sh
@@ -0,0 +1,18 @@
+#! /bin/sh
+
+set -ex
+scriptDir=$(dirname $0 | xargs -i readlink -f {})
+# Check if files in this folder are present in the user home directory
+#  - if yes: backup (move)
+# Then, create a symlink to our file
+for file in $(find ${scriptDir} -maxdepth 1 -name ".*" \! -name "." \! -name ".git"); do
+    origf=~/$(basename ${file})
+    if [ $(readlink ${origf}) = ${file} ]; then
+        continue
+    fi
+    if [ -e ${origf} ]; then
+        mv ${origf} ${origf}.backup
+    fi
+    ln -s ${file} ${origf}
+done
+
