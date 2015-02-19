490bb86bfde98ff47071f0d43202e26b59c49949
Soqaris <soqaris@gmail.com>
2015-02-19 15:02:25 +0100

    vim-airline symbols

--- install.sh
+++ install.sh
@@ -1,10 +1,12 @@
 #! /bin/sh
 
-set -ex
+set -e
 scriptDir=$(dirname $0 | xargs -i readlink -f {})
 # Check if files in this folder are present in the user home directory
 #  - if yes: backup (move)
 # Then, create a symlink to our file
+
+echo "Installing configuration files..."
 for file in $(find ${scriptDir} -maxdepth 1 -name ".*" \! -name "." \! -name ".git"); do
     origf=~/$(basename ${file})
     if [ "$(readlink ${origf})" = "${file}" ]; then
@@ -14,5 +16,6 @@ for file in $(find ${scriptDir} -maxdepth 1 -name ".*" \! -name "." \! -name ".g
         mv ${origf} ${origf}.backup
     fi
     ln -s ${file} ${origf}
+    echo "  - $(basename ${file})"
 done
-
+echo "Done."
