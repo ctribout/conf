194b170e1fd0f17c75ca810f79d4ccbce71d7b73
Soqaris <soqaris@gmail.com>
2015-08-10 17:35:50 +0200

    Powerline fonts installed by default

--- install.sh
+++ install.sh
@@ -2,6 +2,7 @@
 
 set -e
 scriptDir=$(dirname $0 | xargs -i readlink -f {})
+cd ${scriptDir}
 # Check if files in this folder are present in the user home directory
 #  - if yes: backup (move)
 # Then, create a symlink to our file
@@ -18,4 +19,10 @@ for file in $(find ${scriptDir} -maxdepth 1 -name ".*" \! -name "." \! -name ".g
     ln -s ${file} ${origf}
     echo "  - $(basename ${file})"
 done
-echo "Done."
+
+if [ ! -d fonts ]; then
+    echo "Installing fonts..."
+    git clone https://github.com/powerline/fonts
+    ./fonts/install.sh
+    echo "Done."
+fi
