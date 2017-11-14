fe8a35d705cf61f4f95bdac7322814251a078822
Chris Tribout <chris.tribout@gmail.com>
2017-11-14 17:42:10 +0100

    firefox: add userChrome.css file
    This allows to get rid of the Stylish extension I only used to be able
    to have full-length bookmark titles (especially useful for RSS: cut
    titles are painful)
    Hopefully will work with Firefox 57, once I will be forced to moved to it

--- install.sh
+++ install.sh
@@ -1,8 +1,9 @@
-#! /bin/sh
+#! /bin/bash
 
 set -e
-scriptDir=$(dirname $0 | xargs -i readlink -f {})
-cd ${scriptDir}
+# set -x
+scriptDir=$(dirname "$0" | xargs -i readlink -f "{}")
+cd "${scriptDir}"
 # Check if files in this folder are present in the user home directory
 #  - if yes: backup (move)
 # Then, create a symlink to our file
@@ -13,21 +14,23 @@ if [ ! -d ~/.vim/bundle/neobundle.vim ]; then
     ./install_vim_neobundle.sh
 fi
 
-for conf_file_folder in $(find ${scriptDir}/files/ -mindepth 1 -maxdepth 1 -type d); do
+for conf_file_folder in $(find "${scriptDir}/files/" -mindepth 1 -maxdepth 1 -type d); do
     for target_folder in $(${conf_file_folder}/location.sh); do
-        for conf_file in $(find ${conf_file_folder} -mindepth 1 -maxdepth 1 -not -name "location.sh"); do
-            target_file=${target_folder}/$(basename ${conf_file})
-            if [ "$(readlink ${target_file})" = "${conf_file}" ]; then
+        while IFS= read -r conf_file; do
+            target_file=${target_folder}/${conf_file}
+            if [ $(readlink -f "${target_file}") = $(readlink -f "${conf_file_folder}/${conf_file}") ]; then
                 # Already installed
+                echo "  - $(readlink -f "${conf_file_folder}/${conf_file}") already installed"
                 continue
             fi
-            if [ -e ${target_file} -o -L ${target_file} ]; then
+            if [ -e "${target_file}" -o -L "${target_file}" ]; then
                 # File already exists, save it just in case...
-                mv ${target_file} ${target_file}.backup
+                mv "${target_file}" "${target_file}.backup"
             fi
-            ln -s ${conf_file} ${target_file}
-            echo "  - ${conf_file}"
-        done
+            mkdir -p "$(dirname \"${target_file}\")"
+            ln -s ${conf_file_folder}/${conf_file} ${target_file}
+            echo "  - $(readlink -f "${conf_file_folder}/${conf_file}") installed"
+        done < <(cd "${conf_file_folder}" && find . -mindepth 1 -type f -a -not -name "location.sh")
     done
 done
 
