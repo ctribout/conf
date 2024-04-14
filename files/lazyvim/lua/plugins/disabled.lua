6c7fb822fbc915d4f6caf7af7ed19a188c721ccd
Chris Tribout <chris.tribout@gmail.com>
2024-04-14 12:23:00 +0200

    nvim

--- files/lazyvim/lua/plugins/disabled.lua
+++ files/lazyvim/lua/plugins/disabled.lua
@@ -1,12 +1,15 @@
 return {
   -- Annoying input field for commands instead of the bottom bar
   { "folke/noice.nvim", enabled = false },
-  -- Too noisy for nothing useful
+  -- Snippets are too noisy for nothing very useful
   { "rafamadriz/friendly-snippets", enabled = false },
+  { "L3MON4D3/LuaSnip", enabled = false },
   -- Not always convenient to have closing parenthesis added automatically
   { "echasnovski/mini.pairs", enabled = false },
   -- Messes with basic mappings like "S", "t", and no obvious added value
   { "folke/flash.nvim", enabled = false },
   -- Useless (autocloses html tags)
   { "windwp/nvim-ts-autotag", enabled = false },
+  -- colorscheme not used
+  { "folke/tokyonight.nvim", enabled = false },
 }
