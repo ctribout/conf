769afb4b5b87c58f484e05dd282d5050f80a1539
Chris Tribout <chris.tribout@gmail.com>
2026-04-14 12:02:25 +0200

    nvim: fix icon for yaml files with LiterationMonoNerdFont

--- files/neovim/lua/plugins/editor_tools.lua
+++ files/neovim/lua/plugins/editor_tools.lua
@@ -22,6 +22,26 @@ return {
     -- https://github.com/nvim-neo-tree/neo-tree.nvim
     "nvim-neo-tree/neo-tree.nvim",
     branch = "v3.x",
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      "MunifTanjim/nui.nvim",
+      "nvim-tree/nvim-web-devicons",
+    },
+    config = function()
+      require("nvim-web-devicons").set_icon({
+        yaml = {
+          icon = "",
+          color = "#6d8086",
+          name = "Yaml"
+        },
+        yml = {
+          icon = "",
+          color = "#6d8086",
+          name = "Yml"
+        },
+      })
+    end,
+    lazy = false, -- neo-tree will lazily load itself
     cmd = "Neotree",
     keys = {
       {
